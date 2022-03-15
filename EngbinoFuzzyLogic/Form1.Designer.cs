0f829de10e4cd4edc604811b61ffce478491b265
KEI <engbinop@gmail.com>
2022-03-15 16:06:23 +0800

    Fuzzy Logic

---
+++ EngbinoFuzzyLogic/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace AliacFuzzyLogic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(561, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.fileToolStripMenuItem.Text = "Fuzzy Logic";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.loadToolStripMenuItem.Text = "Create Fuzzy Logic";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(187, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(187, 315);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 31);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(22, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount of Detergent";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(22, 322);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dirt on Clothes";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(268, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "fuziffy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(268, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "fuziffy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button3.Location = new System.Drawing.Point(26, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(326, 34);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Defuziffy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button4.Location = new System.Drawing.Point(222, 401);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 68);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Wash Clothes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(374, 267);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(167, 202);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "CLick Here for 1 step process";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(142, 438);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(74, 31);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(20, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Throttle Output";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(142, 399);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(74, 31);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(22, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Level of Dirt";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Quick Description:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 15;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(26, 67);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(515, 182);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.Text = resources.GetString("textBox5.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 497);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Engbino Smart Washers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ EngbinoFuzzyLogic/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DotFuzzy;
+namespace AliacFuzzyLogic
+{
+    public partial class Form1 : Form
+    {
+        FuzzyEngine fe;
+        MembershipFunctionCollection detergent,dirt,throttle;
+        LinguisticVariable clothes_detergent, clothes_dirt, mythrottle;
+        FuzzyRuleCollection myrules;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+        public void setMembers()
+        {
+
+            detergent = new MembershipFunctionCollection();
+            detergent.Add(new MembershipFunction("LOW",0.0,0.0,45.0,50.0));
+            detergent.Add(new MembershipFunction("OK", 45.0, 50.0, 50.0, 55.0));
+            detergent.Add(new MembershipFunction("HIGH", 50.0, 55.0, 100.0, 100.0));
+            clothes_detergent = new LinguisticVariable("DETERGENT", detergent);
+
+
+            dirt = new MembershipFunctionCollection();
+            dirt.Add(new MembershipFunction("DOWN", 0.0, 0.0, 0.0, 0.0));
+            dirt.Add(new MembershipFunction("LEVEL", 0.0, 1.0, 1.0, 5.0));
+            dirt.Add(new MembershipFunction("UP", 0.0, 5.0, 10.0, 10.0));
+            clothes_dirt = new LinguisticVariable("DIRT", dirt);
+
+            throttle = new MembershipFunctionCollection();
+            throttle.Add(new MembershipFunction("LOW",0.0,0.0,2.0,4.0));
+            throttle.Add(new MembershipFunction("LM", 2.0, 4.0, 4.0, 6.0));
+            throttle.Add(new MembershipFunction("MED", 4.0, 6.0, 6.0, 8.0));
+            throttle.Add(new MembershipFunction("HM", 6.0, 8.0, 8.0, 10.0));
+            throttle.Add(new MembershipFunction("HIGH", 8.0, 10.0, 10.0, 10.0));
+            mythrottle = new LinguisticVariable("THROTTLE", throttle);
+
+            
+        
+        }
+
+        public void setRules()
+        {
+          myrules = new FuzzyRuleCollection();
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS HIGH) AND (DIRT IS UP) THEN THROTTLE IS HIGH"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS HIGH) AND (DIRT IS LEVEL) THEN THROTTLE IS HIGH"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS HIGH) AND (DIRT IS DOWN) THEN THROTTLE IS HIGH"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS OK) AND (DIRT IS UP) THEN THROTTLE IS MED"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS OK) AND (DIRT IS LEVEL) THEN THROTTLE IS HM"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS OK) AND (DIRT IS DOWN) THEN THROTTLE IS HIGH"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS LOW) AND (DIRT IS UP) THEN THROTTLE IS MED"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS LOW) AND (DIRT IS LEVEL) THEN THROTTLE IS LM"));
+          myrules.Add(new FuzzyRule("IF (DETERGENT IS LOW) AND (DIRT IS DOWN) THEN THROTTLE IS LOW"));
+        }
+
+        public void setFuzzyEngine()
+        {
+            fe = new FuzzyEngine();
+            fe.LinguisticVariableCollection.Add(clothes_detergent);
+            fe.LinguisticVariableCollection.Add(clothes_dirt);
+            fe.LinguisticVariableCollection.Add(mythrottle);
+            fe.FuzzyRuleCollection = myrules;
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void defuziffyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setMembers();
+            setRules();
+            //setFuzzyEngine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clothes_detergent.InputValue=(Convert.ToDouble(textBox1.Text));
+            clothes_detergent.Fuzzify("OK");
+            
+            
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clothes_dirt.InputValue = (Convert.ToDouble(textBox2.Text));
+            clothes_dirt.Fuzzify("LEVEL");
+            
+        }
+
+        public void fuziffyvalues()
+        {
+            clothes_detergent.InputValue = (Convert.ToDouble(textBox1.Text));
+            clothes_detergent.Fuzzify("LOW");
+            clothes_dirt.InputValue = (Convert.ToDouble(textBox2.Text));
+            clothes_dirt.Fuzzify("DOWN");
+        
+        }
+        public void defuzzy()
+        {
+            setFuzzyEngine();
+            fe.Consequent = "THROTTLE";
+            textBox3.Text = "" + fe.Defuzzify().ToString("0.00");
+        }
+
+        public void washclothes()
+        {
+
+            double detergent = Convert.ToDouble(textBox1.Text);
+            double throttle = Convert.ToDouble(textBox3.Text);
+            double dirt = Convert.ToDouble(textBox2.Text);
+            double washed;
+
+            double newDetergent = ((1 - 0.5) * (detergent)) + (throttle - (0.5 * dirt));
+
+            if (dirt == 0)
+                washed = 0;
+            else
+                washed = ((1 - 0.1) * (dirt)) + (detergent - (0.1 * throttle));
+
+            textBox1.Text = "" + newDetergent.ToString("0.00");
+            textBox4.Text = "" + washed.ToString("0.00");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            setFuzzyEngine();
+            fe.Consequent = "THROTTLE";
+            textBox3.Text = "" + fe.Defuzzify().ToString("0.00");
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            washclothes();
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setMembers();
+            setRules();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fuziffyvalues();
+            defuzzy();
+            washclothes();
+        }
+
+       
+    }
+}
